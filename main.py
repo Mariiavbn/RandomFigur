db380d1375dfd64a9e58db0e3d437d00c8524f07
Mariiavbn <157039167+Mariiavbn@users.noreply.github.com>
2024-04-07 10:20:03 +0500

    main.py

--- main.py
+++ main.py
@@ -14,15 +14,12 @@ while True:
     screen.fill(pg.Color(r, g, b))
 
     type = random.randint(0, 3)
-    rectelips = (r, g, 158, b)
     if type == 0:
-        #pg.draw.rect(screen, pg.Color(r, b, g), (r, g, 100, 58), 4)
-        pg.draw.rect(screen, pg.Color(r, b, g), rectelips)
+        pg.draw.rect(screen, pg.Color(r, b, g), (r, g, 100, 58), 4)
     if type == 1:
         pg.draw.circle(screen, pg.Color(g, r, b), (g, r), 200)
     if type == 2:
-        #pg.draw.ellipse(screen, pg.Color(g, r, b), (b, r, 70, 100))
-        pg.draw.ellipse(screen, pg.Color(g, r, b), rectelips)
+        pg.draw.ellipse(screen, pg.Color(g, r, b), (b, r, 70, 100))
     if type == 3:
         pg.draw.polygon(screen, pg.Color(g, b, r), ((g, b), (450, 70), (53, 62)))
 
